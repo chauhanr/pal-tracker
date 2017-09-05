ddc0d00e2fab5d611c52f8ea5a6de58db77bb9fd
Ritesh <ritesh.chauhan@hcl.com>
2017-09-05 12:41:05 +0530

    adding manifests and deployment details

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -2,12 +2,22 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+
+
 
 @RestController
 public class WelcomeController {
+    private String message;
+
+
+    public WelcomeController(@Value("${welcome_message}") String message){
+        this.message = message;
+    }
 
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return message;
     }
 }
+
